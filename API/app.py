d5768dba820d2c643ea2aae2b82cc5db847a7c66
DarkVengeres <gabmayrand@hotmail.ca>
2022-04-12 16:06:01 -0400

    feat: init app.py and mysql connection

--- API/app.py
+++ API/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+
+#Configure db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'application_parking'
+
+mysql = MySQL(app)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if(request.method=='GET'):
-        data="hello world"
-        return jsonify({'data':data})
+        cur = mysql.connection.cursor()
+        utilisateurs = cur.execute("SELECT * FROM utilisateur")
+        utilisateurs = cur.fetchall()
+        return jsonify(utilisateurs)
 
 if __name__ == '__main__':
     app.run(debug=True)
