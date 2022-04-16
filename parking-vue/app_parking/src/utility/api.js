8e31809c1f3c0eaebd0eef28ae20d87289a794f3
DarkVengeres <gabmayrand@hotmail.ca>
2022-04-16 14:55:55 -0400

    stash

--- parking-vue/app_parking/src/utility/api.js
+++ parking-vue/app_parking/src/utility/api.js
@@ -1,26 +1,31 @@
 export class API {
     constructor() {
-      this.url = "https://127.0.0.1:5000/";
+      this.url = "http://127.0.0.1:5000/";
       this.response = "";
     }
     
       async getAPIDataSecure(query, HTTPOptions) {
         try {
-          let JSONresponse = await fetch(this.url + query, HTTPOptions);
-          this.response = await JSONresponse.json();
+            console.log( query+HTTPOptions);
+            let JSONresponse = await fetch(this.url + query, HTTPOptions);
+            this.response = await JSONresponse.json();
         } catch (error) {
-          console.log(error);
-          this.response = "error";
+            console.error(error);
+            this.response = "error";
         }
       }
 
     async getParkingList() {
         let HTTPOptions = {
-            headers: {},
+            headers: {
+                'Access-Control-Allow-Origin': 'origin-list',
+                'Access-Control-Allow-Methods': 'GET',
+            },
             method: "GET",
             'Content-Type': 'application/json'
         };
         let URL = "parkingList";
+        console.log(this.url + URL, HTTPOptions);
         await this.getAPIDataSecure(URL, HTTPOptions);
     }
 }  
