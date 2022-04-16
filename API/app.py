3ce4fdee17fcb1d0b400bd2ceb29e97c8530f488
Nicolas Nadeau <nico.pou@hotmail.com>
2022-04-16 15:28:08 -0400

    test3

--- API/app.py
+++ API/app.py
@@ -21,10 +21,10 @@ def home():
     elif(request.method == 'POST'):
         data = request.get_json()
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO gerer (id_stationnement, id_utilisateur) VALUES (%s, %s)",
+        cur.execute("INSERT INTO stationnement (id_stationnement, prix, longueur, largeur, hauteur, emplacement, jours_d_avance, date_fin) VALUE (%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (data['id'], data['prix'], data['longueur'], data['largeur'], data['hauteur'], data['emplacement'], data['jours_d_avance'], data['date_fin']))
+        cur.execute("INSERT INTO gerer (id_stationnement, id_utilisateur) VALUE (%s, %s)",
                     (data['id'], data['id_utilisateur']))
-        cur.execute(
-            "INSERT INTO Locateur (id_utilisateur, cote) VALUE (%s, NULL)", [data['id_utilisateur']])
         mysql.connection.commit()
         return 'OK'
 
